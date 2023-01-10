554b120702588ce72558d6d4d9eca0bfd59686ba
kamrannar <kamrann.nariman@gmail.com>
2023-01-10 16:58:42 +0400

    url shortener added

--- core/views.py
+++ core/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from .models import Project, Contact
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+import pyshorteners
 
 # Create your views here.
 
 def index(request):
     projects=Project.objects.all()
-    if request.method=="POST":
+    if request.method=="POST" and 'link' in request.POST:
+        link = request.POST['link']
+        print(link)
+        short = pyshorteners.Shortener()
+        x = short.tinyurl.short(link)
+        messages.success(request,'Here is the short url:  '+x)
+        return HttpResponseRedirect('/')
+
+    if request.method=="POST" and 'contact`' in request.POST:
         name=request.POST['name']
         email=request.POST['email']
         subject=request.POST['subject']
