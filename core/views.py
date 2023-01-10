60ed4342086f04da84290d27c8f5f3735624a239
kamrannar <kamrann.nariman@gmail.com>
2023-01-10 16:28:38 +0400

    done

--- core/views.py
+++ core/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Contact
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
 # Create your views here.
 
 def index(request):
     projects=Project.objects.all()
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        subject=request.POST['subject']
+        message=request.POST['message']
+        contact = Contact.objects.create(name=name,email=email,
+        subject=subject,message=message)
+        contact.save()
+        messages.success(request, 'Message successfully sent')
+        return HttpResponseRedirect('/')
+
     return render(request, 'index.html',{'projects':projects})
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MESSAGE_TAGS = {
+        messages.DEBUG: 'secondary',
+        messages.INFO: 'info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'warning',
+        messages.ERROR: 'danger',
+}   
